0338ca77c86da30c4640efbcf4c77455a524c59d
齋藤程 <zhaitengcheng4@gmail.com>
2020-05-30 11:56:40 +0900

    User_model&controller&&sessions_controller-create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  root 'top/index'
+
+
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/new'
+  get 'users/create'
+  get 'users/me'
+  root 'top#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
